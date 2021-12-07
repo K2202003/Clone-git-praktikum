9a77245c7f8fca95b1b4a4754cbd9b5628117cb2
AxVsel <assperacus77@gmail.com>
2021-12-07 23:14:50 +0800

    Commit ke branch baru

--- NewRepoProject/NewRepoProject/Program.cs
+++ NewRepoProject/NewRepoProject/Program.cs
@@ -8,6 +8,7 @@ namespace NewRepoProject
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Pemograman lanjut S1 Informatika");
+            Console.WriteLine("Materi Praktikum Git");
         }
     }
 }
