c31de2f71746e7e0eaa2e1754a9f052d1d13d057
K2202003 <kelompokdua20s1if03@gmail.com>
2021-12-08 02:02:11 +0800

    MUH. YAUMIL AKSAH HAMID 20.11.3480

--- NewRepoProject/NewRepoProject/Program.cs
+++ NewRepoProject/NewRepoProject/Program.cs
@@ -10,6 +10,7 @@ namespace NewRepoProject
             Console.WriteLine("Pemograman lanjut S1 Informatika");
             Console.WriteLine("Materi Praktikum Git");
             Console.WriteLine("Kelompok 2 Pemograman Lanjut 20-S1IF-03");
+            Console.WriteLine("edit 1 MUH. YAUMIL AKSAH HAMID 20.11.3480");
         }
     }
 }
