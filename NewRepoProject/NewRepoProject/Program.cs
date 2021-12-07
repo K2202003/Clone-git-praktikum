d4bd64e2b4a87196690a211f75128bb7900556b6
K2202003 <kelompokdua20s1if03@gmail.com>
2021-12-08 01:39:20 +0800

    Kelompok 2 clone git MUH. YAUMIL AKSAH HAMID

--- NewRepoProject/NewRepoProject/Program.cs
+++ NewRepoProject/NewRepoProject/Program.cs
@@ -9,6 +9,7 @@ namespace NewRepoProject
             Console.WriteLine("Hello World!");
             Console.WriteLine("Pemograman lanjut S1 Informatika");
             Console.WriteLine("Materi Praktikum Git");
+            Console.WriteLine("Kelompok 2 Pemograman Lanjut 20-S1IF-03");
         }
     }
 }
