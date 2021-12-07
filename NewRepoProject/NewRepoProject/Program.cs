fdda7ce07f498f6d3d0da453bcf3513436c65a28
AxVsel <assperacus77@gmail.com>
2021-12-07 23:07:26 +0800

    Merubah Program.cs

--- NewRepoProject/NewRepoProject/Program.cs
+++ NewRepoProject/NewRepoProject/Program.cs
@@ -7,6 +7,7 @@ namespace NewRepoProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Pemograman lanjut S1 Informatika");
         }
     }
 }
